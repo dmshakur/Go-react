d3387cd1a185a758a6dc6e895fe464fd020448ef
D'angelo <dangeloshakur@gmail.com>
2019-05-16 10:47:56 -0500

    Updates

---
+++ precache-manifest.e87ece41ffa3657fa2bed6e5b09037a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "07ea2c54ec80bf36a489",
+    "url": "/Go-react/static/css/main.ca781d32.chunk.css"
+  },
+  {
+    "revision": "07ea2c54ec80bf36a489",
+    "url": "/Go-react/static/js/main.8f59f33b.chunk.js"
+  },
+  {
+    "revision": "0bdae3e7c9b53f59dea9",
+    "url": "/Go-react/static/js/runtime~main.c2042d6e.js"
+  },
+  {
+    "revision": "ad4e95176398153ee6a4",
+    "url": "/Go-react/static/js/2.c35de49c.chunk.js"
+  },
+  {
+    "revision": "4c850590fb262ded9dcbce9b31f32532",
+    "url": "/Go-react/static/media/go.4c850590.png"
+  },
+  {
+    "revision": "317c83ea61eab172f8a1dad69e286ebe",
+    "url": "/Go-react/static/media/Ailerons.317c83ea.otf"
+  },
+  {
+    "revision": "66f74ee5ade7939273de5355d19f2328",
+    "url": "/Go-react/static/media/Accent.66f74ee5.ttf"
+  },
+  {
+    "revision": "e5140f67e67c3cee84b115af1a0ce1a5",
+    "url": "/Go-react/index.html"
+  }
+];
