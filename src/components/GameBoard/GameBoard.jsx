c398e8138e2d34e1a526b1333b2f1b01524f2985
D'angelo <dangeloshakur@gmail.com>
2019-05-17 19:55:45 -0500

    Made significant revisions to GameBoard.jsx, just bug fixes

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -4,19 +4,19 @@ import Stone from '../Stone/Stone'
 import styles from './GameBoard.module.css'
 
 const difficulty = { // Subtract one from the last number when used to set the white reserves
-  easy: [81, {height: 324, width: 324}, 41],
-  medium: [169, {height: 468, width: 468}, 85],
-  go: [361, {height: 684, width: 684}, 181]
+  easy: [81, {height: 324, width: 324}, 41, 9],
+  medium: [169, {height: 468, width: 468}, 85, 13],
+  go: [361, {height: 684, width: 684}, 181, 19]
 }
 
 const tactInfo = {
   piece: "free",   // Either black or white
   chainId: null,   // corresponds to all the stones in a single chain
   openLinks: null, // number up to three, corresponds to the amount of trues in the Iff's Might need to get rid of this in favor of a more efficent operation
-  topIff: null,
-  rightIff: null,
-  bottomIff: null,
-  leftIff: null
+  topIff: "free",
+  rightIff: "free",
+  bottomIff: "free",
+  leftIff: "free"
 }
 
 class GameBoard extends Component {
@@ -50,7 +50,7 @@ class GameBoard extends Component {
     let tempBoardPointsTact = {}
     for (let i = 0; i < num; i++) {
       tempBoardPointsTact[i] = tactInfo
-      tempBoardPoints.push(<Point handlePointClick={this.handlePointClick.bind(this, i)} pos={i} />)
+      tempBoardPoints.push(<Point handlePointClick={(e) => this.handlePointClick(i, e)} key={i} pos={i} />)
     }
     this.setState({
       boardPointsTact: tempBoardPointsTact,
@@ -70,82 +70,93 @@ class GameBoard extends Component {
   }
 
   boardRender = e => {
-    let tempBoardPointsTact = this.state.boardPointsTact
-    let tempBoardPoints = this.state.boardPoints
-
-    tempBoardPointsTact.map(obj => {
-      for (let i = 0; i < difficulty[this.props.gameDiff][0]; i++) {
-        if (obj[i].piece !== "free") {
-           tempBoardPoints[i] = <Point onClick={this.handlePointClick} pos={i}><Stone className={styles[obj.piece]} /></Point>
-        }
+    let x, tempBoardPoints = this.state.boardPoints
+    // Object.keys(tempBoardPointsTact).map(obj => {
+    //   for (let i = 0; i < difficulty[this.props.gameDiff][0]; i++) {
+    //     if (obj[i].piece !== "free") {
+    //        tempBoardPoints[i] = <Point onClick={this.handlePointClick} pos={i}><Stone className={styles[obj.piece]} /></Point>
+    //     }
+    //   }
+    // })
+    for (x in tempBoardPoints) {
+      if (this.state.boardPointsTact[x].piece !== "free") {
+        tempBoardPoints[x] = <Point><Stone className={styles[this.state.boardPointsTact[x].piece]} /></Point>
       }
-    })
-    this.setState({boardPointsTact: tempBoardPointsTact, boardPoints: tempBoardPoints})
+    }
+    this.setState({boardPoints: tempBoardPoints})
   }
 
-  handleChainLinks = (pointTact, pos) => {
-    let mergeChains = []
-    let tempBoardPointsTact = this.state.boardPointsTact
-    let tempPointTact = pointTact // This is just the object of the single point clicked where a piece should be placed
+  handleChainLinks = (pointTact, arr) => { // Needs work
     // Finding any friendly chains, then pushing them to the above array
-    if (this.state.boardPointsTact[pos - 1].piece === this.state.playerTurn) {
-      mergeChains.push(this.state.boardPointsTact[pos - 1].chainId)
-    }
-    if (this.state.boardPointsTact[pos - 19].piece === this.state.playerTurn) {
-      mergeChains.push(this.state.boardPointsTact[pos - 19].chainId)
-    }
-    if (this.state.boardPointsTact[pos + 1].piece === this.state.playerTurn) {
-      mergeChains.push(this.state.boardPointsTact[pos + 1].chainId)
-    }
-    if (this.state.boardPointsTact[pos + 19].piece === this.state.playerTurn) {
-      mergeChains.push(this.state.boardPointsTact[pos + 19].chainId)
-    }
+    // convert all links to have the same chainId then delete all the
+    // old links from a copy of the chainId array, then replace the old with the new chainId
+
+    let mergeChains = [], tempChains = this.state.chains, x, tempBoardPointsTact // Getting all the chain ID's here
+    arr.map(num => {
+      console.log(this.state.boardPointsTact[num])
+      if (this.state.boardPointsTact[num].piece === this.state.playerTurn)
+        mergeChains.push(this.state.boardPointsTact[num].chainId)
+    })
+
+    if (pointTact.chainId !== null) mergeChains.push(pointTact.chainId)
+
     if (mergeChains.length > 1) {
-      // convert all links to have the same chainId then delete all the
-      // old links from a copy of the chainId array, then replace the old with the new chainId
-      mergeChains.map(cId => {
-        tempBoardPointsTact.map(ob => {
-          if (ob.chainId === cId) ob.chainId = this.state.currentChain + 1
-        })
+      mergeChains.map(chainId => {
+        for (x in tempBoardPointsTact) {
+          if (tempBoardPointsTact.chainId === chainId) {
+            tempBoardPointsTact.chainId = this.state.currentChain + 1
+            if (tempChains[0] !== this.state.currentChain + 1) tempChains.unshift(this.state.currentChain + 1)
+          }
+        }
       })
-      this.setState({currentChain: this.state.currentChain + 1, boardPointsTact: tempPointTact})
-      return tempPointTact
+      this.setState({currentChain: this.state.currentChain + 1, chains: tempChains})
+      return pointTact
     } else if (mergeChains.length === 1) {
-      tempPointTact.chainId = mergeChains[0]
-      return tempPointTact
+      pointTact.chainId = mergeChains[0]
+      return pointTact
     }
   }
 
   handleTactChange = (pointTact, pos) => { // point represents an object holding all the tact data
     // Check surrounding areas links etc to determine how the state of the tactInfo should change
-    let tempPointData = this.state.boardPointsTact[pos]
-
     // Setting the IFF's below
-    pos - 1 < 0    ? tempPointData.rightIff  = this.state.waitingPlayer : tempPointData.rightIff  = this.state.boardPointsTact[pos - 1].piece
-    pos - 19 < 0   ? tempPointData.toptIff   = this.state.waitingPlayer : tempPointData.topIff    = this.state.boardPointsTact[pos - 19].piece
-    pos + 1 > 360  ? tempPointData.leftIff   = this.state.waitingPlayer : tempPointData.leftIff   = this.state.boardPointsTact[pos + 1].piece
-    pos + 19 > 360 ? tempPointData.bottomIff = this.state.waitingPlayer : tempPointData.bottomIff = this.state.boardPointsTact[pos + 19].piece
+    // Need to further edit below, need to edit the IFF's of pieces adjacent
+    const num = difficulty[this.props.gameDiff][3]
 
-    if (tempPointData.rightIff === this.state.playerTurn ||
-        tempPointData.leftIff === this.state.playerTurn ||
-        tempPointData.topIff === this.state.playerTurn ||
-        tempPointData.bottomIff === this.state.playerTurn) {
-      this.handleChainLinks(tempPointData, pos)
+    pos - 1 < 0 ?
+      pointTact.rightIff = this.state.waitingPlayer
+      : pointTact.rightIff = this.state.boardPointsTact[pos - 1].piece
+    pos - num < 0 ?
+      pointTact.toptIff = this.state.waitingPlayer
+      : pointTact.topIff = this.state.boardPointsTact[pos - num].piece
+    pos + 1 > (difficulty[this.props.gameDiff][0] - 1) ?
+      pointTact.leftIff = this.state.waitingPlayer
+      : pointTact.leftIff = this.state.boardPointsTact[pos + 1].piece
+    pos + num > (difficulty[this.props.gameDiff][0] - 1) ?
+      pointTact.bottomIff = this.state.waitingPlayer
+      : pointTact.bottomIff = this.state.boardPointsTact[pos + num].piece
+
+    let arr = []
+    if (pointTact.rightIff === this.state.playerTurn) arr.push(pos - 1)
+    if (pointTact.topIff === this.state.playerTurn) arr.push(pos - num)
+    if (pointTact.leftIff === this.state.playerTurn) arr.push(pos + 1)
+    if (pointTact.bottomIff === this.state.playerTurn) arr.push(pos + num)
+    if (arr.length > 0) {
+      this.handleChainLinks(pointTact, arr)
+      return pointTact
     } else {
-      tempPointData.chainId = this.state.currentChain + 1
+      pointTact.chainId = this.state.currentChain + 1
       this.setState({currentChain: this.state.currentChain + 1})
-      return tempPointData
+      return pointTact
     }
   }
 
-  handlePointClick = (e, pos) => { // Function that is triggered on click of a div where a go piece is placeable
-    e.preventDefault()
-    console.log(e.target.piece)
-    if (e.target.piece === "black" || e.target.piece === "white") return
-    let tempBoardPoints = this.state.boardPoints
+  handlePointClick = (pos, e) => { // Function that is triggered on click of a div where a go piece is placeable
+    if (this.state.boardPointsTact[pos].piece === "black" || this.state.boardPointsTact[pos].piece === "white") return
     let tempBoardPointsTact = this.state.boardPointsTact
-    tempBoardPointsTact[e.target.pos] = this.handleTactChange(tempBoardPointsTact[e.target.pos], e.target.pos)
-    // this.setState({boardPointsTact: tempBoardPointsTact, boardPoints: tempBoardPoints})
+    tempBoardPointsTact[pos].piece = this.state.playerTurn
+    tempBoardPointsTact[pos] = this.handleTactChange(tempBoardPointsTact[pos], pos)
+    this.setState({boardPointsTact: tempBoardPointsTact})
     this.handleTurnChange()
     this.boardRender()
   }
